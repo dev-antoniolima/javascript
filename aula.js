bdbb63b0c28f08b1aec1d21197222bc328065dd8
Antonio Lima <antonio.lima@metatron.com.br>
2021-03-09 15:43:01 -0300

    Objetos

--- aula.js
+++ aula.js
@@ -1,24 +1,53 @@
-// (1) - Tipos de Variáveis //
-let idade = 10;
-const nome = "Antonio Lima";
-let profissao = "programador";
+// // (1) - Tipos de Variáveis //
+// let idade = 10;
+// const nome = "Antonio Lima";
+// let profissao = "programador";
 
-console.log(idade,nome,profissao);
-//-------------------------------//
+// console.log(idade,nome,profissao);
+// //-------------------------------//
 
-// (2) - Tipos Primitivos //
-let nome = "Rafael"; //string literal = letras e números juntos.
-let idade = 25; //number = valor númeral.
-let estaAprovado = true; //boolean - condição verdadeira/falsa.
-let sobrenome: undefined; //Undefined.
-let corSelecionado = null // Resetar Valor.
-//-------------------------------//
+// // (2) - Tipos Primitivos //
+// let nome = "Rafael"; //string literal = letras e números juntos.
+// let idade = 25; //number = valor númeral.
+// let estaAprovado = true; //boolean - condição verdadeira/falsa.
+// let sobrenome: undefined; //Undefined.
+// let corSelecionado = null // Resetar Valor.
+// //-------------------------------//
 
-// (3) - Tipagem Dinâmica //
-let nome = "Rafael"; //typeof = string
-let idade = 25; //typeof = number
-let estaAprovado = true; //typeof = boolean
-let sobrenome: undefined; //typeof = undefined
-let corSelecionado = null //typeof = object
-//Javascript identifica o tipo de variável sem a necessidade de especificar-la.//
+// // (3) - Tipagem Dinâmica //
+// let nome = "Rafael"; //typeof = string
+// let idade = 25; //typeof = number
+// let estaAprovado = true; //typeof = boolean
+// let sobrenome: undefined; //typeof = undefined
+// let corSelecionado = null //typeof = object
+// //Javascript identifica o tipo de variável sem a necessidade de especificar-la.//
+// //-------------------------------//
+
+// (4) - Objetos //
+let pessoa = { 
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'de Souza'
+};
+
+console.log(pessoa);
+
+let carro = { 
+    modelo: 'Tesla',
+    ano: 1955,
+    placaVerificada: true,
+    numPlaca: '5u14abc'
+};
+
+console.log(carro);
+
+let produto = { 
+    nome: 'Caixa de som',
+    numIdentificacao: 55,
+    estoque: true,
+    modelo: '9839348934'
+};
+
+console.log(produto);
 //-------------------------------//
