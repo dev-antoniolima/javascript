4ab195efe1e65ca2be4b2f998e8101b4889d859a
Antonio Lima <antonio.lima@metatron.com.br>
2021-03-09 14:59:48 -0300

    01 - Tipos de Variáveis

---
+++ aula.js
@@ -0,0 +1,8 @@
+// (1) - Tipos de Variáveis //
+let idade = 10;
+const nome = "Antonio Lima";
+let profissao = "programador";
+
+console.log(idade,nome,profissao);
+//-------------------------------//
+
