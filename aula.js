b1855a85e8b1662e7c96c73646c01036934de23b
Antonio Lima <antonio.lima@metatron.com.br>
2021-03-09 15:14:31 -0300

    03 - Tipagem Dinâmica

--- aula.js
+++ aula.js
@@ -13,3 +13,12 @@ let estaAprovado = true; //boolean - condição verdadeira/falsa.
 let sobrenome: undefined; //Undefined.
 let corSelecionado = null // Resetar Valor.
 //-------------------------------//
+
+// (3) - Tipagem Dinâmica //
+let nome = "Rafael"; //typeof = string
+let idade = 25; //typeof = number
+let estaAprovado = true; //typeof = boolean
+let sobrenome: undefined; //typeof = undefined
+let corSelecionado = null //typeof = object
+//Javascript identifica o tipo de variável sem a necessidade de especificar-la.//
+//-------------------------------//
