45e3964f76d953e6665bb30e43aa4e46907735e4
Antonio Lima <antonio.lima@metatron.com.br>
2021-03-09 15:05:30 -0300

    02 - Tipos Primitivos

--- aula.js
+++ aula.js
@@ -6,3 +6,10 @@ let profissao = "programador";
 console.log(idade,nome,profissao);
 //-------------------------------//
 
+// (2) - Tipos Primitivos //
+let nome = "Rafael"; //string literal = letras e números juntos.
+let idade = 25; //number = valor númeral.
+let estaAprovado = true; //boolean - condição verdadeira/falsa.
+let sobrenome: undefined; //Undefined.
+let corSelecionado = null // Resetar Valor.
+//-------------------------------//
