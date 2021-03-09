e764b85d0ac79d87c3ec13e1f08371a0b85394e9
Antonio Lima <antonio.lima@metatron.com.br>
2021-03-09 16:02:07 -0300

    5 - Arrays

--- aula.js
+++ aula.js
@@ -51,3 +51,12 @@ let produto = {
 
 console.log(produto);
 //-------------------------------//
+
+// (4) - Arrays //
+let familia = [26, 45, 50, 17]
+console.log(familia.length);
+console.log(familia[2]);
+//indice identificado pelo número 2 (posição na array)
+
+//Array: Conjunto de dados que pode ser acessado por um índice.
+//-------------------------------//
