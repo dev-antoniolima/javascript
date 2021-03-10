a12d7322efc14875fcdcd24b8622700fb867c2c4
Antonio Lima <antonio.lima@metatron.com.br>
2021-03-10 13:35:11 -0300

    7 - Tipos de Funções

--- aula.js
+++ aula.js
@@ -72,3 +72,22 @@ console.log(corSite);
 resetaCor("Verde","claro");
 console.log(corSite);
 //-------------------------------//
+
+
+// (6) - Tipos de Funções //
+
+// Realiza uma tarefa e não retorna nada.
+function dizerNome(){
+    console.log('jhonatan');
+}
+dizerNome();
+///////////////////////////////////////////
+
+// Realiza uma tarefa e retona seu valor em uma variável.
+function MultiplicarPorDois(valor){
+    return valor * 2;
+}
+
+let resultado = MultiplicarPorDois(5);
+console.log(resultado);
+//-------------------------------//
