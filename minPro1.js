ce1f437962d839c7990c7e79d5d307df7db79e40
Antonio Lima <antonio.lima@metatron.com.br>
2021-03-14 18:54:50 -0300

    Mini Projeto (1) - Trocando Valores de Variáveis

---
+++ minPro1.js
@@ -0,0 +1,12 @@
+// Mini Projeto (1) - Trocando Valores de Variáveis //
+
+let a = "vermelho";
+let b = "azul";
+let varTroca;
+
+varTroca = a;
+a = b
+b= varTroca;
+
+console.log(a);
+console.log(b);
