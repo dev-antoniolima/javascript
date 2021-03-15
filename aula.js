2cbf1276c55e6af388baaf2e60f0be572ec7533c
Antonio Lima <antonio55senac@gmail.com>
2021-03-15 14:15:48 +0000

    9 - Condicionais switch case

--- aula.js
+++ aula.js
@@ -223,6 +223,25 @@ console.log("Boa noite");
 
 //Switch .. case
 
+let permissao; // comum, gerente, diretor
+permissao = 'gerente';
+
+switch(permissao) {
+    case 'comum':
+        console.log('usuário comum');
+        break;
+
+         case 'gerente':
+        console.log('usuário gerente');
+        break;
+
+         case 'diretor':
+        console.log('usuário diretor');
+        break;
+
+
+        default: console.log("Usuário não reconhecido");
+}
 
 
 
