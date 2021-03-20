7b3bb626b17241c08efa17eb0aea63500ad0505f
Antonio Lima <antonio55senac@gmail.com>
2021-03-20 00:33:38 +0000

    Mini Projeto(2)

---
+++ Mini-Projetos/minPro2.js
@@ -0,0 +1,12 @@
+//  Mini Projeto (2) - Escrever uma função que usa 2 números e retorna o maior entre eles.
+
+// ? = se condição for verdadeira
+// : = se condição for falsa
+
+let valorMaior = max(5,9)
+console.log(valorMaior);
+
+function max(valor1,valor2) {
+   return valor1 > valor2 ? numero1 : numero2 ;
+}
+
