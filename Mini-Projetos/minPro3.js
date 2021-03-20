d69f289c706d338013ccd07db9409513852035fc
Antonio Lima <antonio55senac@gmail.com>
2021-03-20 00:59:16 +0000

    Mini Projeto 3 - FizzBuzz

---
+++ Mini-Projetos/minPro3.js
@@ -0,0 +1,28 @@
+// Mini Projeto (3) - Diversas Condições FizzBuzz
+
+// Divisivel por 3 => Fizz
+// Divisivel por 5 => Buzz
+// Divisivel por 3 e 5 => FizzBuzz
+// Não divisivel por 3 e 5 => entrada
+// Não é um número => 'Não é um número'
+const resultado = fizzBuzz(60);
+console.log(resultado)
+
+function fizzBuzz(entrada){
+    if ( typeof entrada !== 'number') 
+    return ("Não é um número");
+    
+    if( entrada % 3 === 0 && entrada % 5 === 0)
+    return 'FizzBuzz';
+
+
+    if ( entrada % 3 === 0)
+    return 'Fizz';
+
+    if( entrada % 5 === 0)
+    return 'Buzz';
+
+  
+
+    return entrada;
+}
