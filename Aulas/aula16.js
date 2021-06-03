fc19fa73161124c6d388863451a07b017ad164b9
Antonio Lima <antonio.lima@metatron.com.br>
2021-06-03 15:51:24 -0300

    Aula 16 - Template Literal

---
+++ Aulas/aula16.js
@@ -0,0 +1,10 @@
+// (16) - Template Literal //
+
+// O Template literal mantém a formatação do texto original.
+const nome = 'Antonio Lima'
+const email =
+`Olá, Obrigado por se inscrever
+atenciosamente, ${nome}
+`
+
+console.log(email);
