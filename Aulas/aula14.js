82e9325efab3be43852dc4a6e1e6383f14ea2d24
Antonio Lima <antonio.lima@metatron.com.br>
2021-06-03 15:23:59 -0300

    Aula 14 - Math

---
+++ Aulas/aula14.js
@@ -0,0 +1,13 @@
+// (14) - MATH //
+
+// Gera um número aleatório ente 0 e 1.
+let valor1 = Math.random();
+console.log(valor1);
+
+// Exibe o maior valor entre os passados como paramêtro.
+let valor2 = Math.max(3,8,10);
+console.log(valor2);
+
+// Exibe o menor valor entre os passados como paramêtro.
+let valor3 = Math.min(3,8,10);
+console.log(valor3);
