db151df525fdd12561d1071c24585334e2c4c76b
Antonio Lima <antonio.lima@metatron.com.br>
2021-06-03 15:36:40 -0300

    Aula 15 - Tipos de String

---
+++ Aulas/aula15.js
@@ -0,0 +1,12 @@
+// (15) - STRING //
+
+// Tipo Primitivo
+const mensagem = 'Minha primeira mensagem';
+
+// Tipo Objeto
+const outraMensagem = new String('Minha nova mensagem');
+
+let tipoPrimitivo = typeof(mensagem);
+let tipoObjeto = typeof(outraMensagem);
+
+console.log(tipoPrimitivo, tipoObjeto);
