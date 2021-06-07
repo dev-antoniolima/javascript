31bea16103aad3cee6d0e1b04e01962f9a3da871
Antonio Lima <antonio55senac@gmail.com>
2021-06-07 22:54:26 +0000

    Aula 19 - Adicionando elementos a um array

---
+++ Aulas/aula19.js
@@ -0,0 +1,27 @@
+// (19) - Introdução a Arrays //
+
+// Como adicionar elementos
+
+const numeros = [1,2,3];
+
+// propriedade que insere valores no início de um array.
+numeros.unshift('inicio');
+console.log(numeros);
+
+// propriedade que insere valores no meio de um array.
+numeros.splice(1,0,'meio');
+console.log(numeros);
+
+// propriedade que insere valores no final de um array.
+numeros.push('final');
+console.log(numeros);
+
+
+
+
+
+// Como encontrar elementos
+// Como remover elementos
+// Como dividir elementos
+// Como dividir Arrays
+// como combinar Arrays
