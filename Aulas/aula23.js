d886861d1f78fabdb85582bc5e7db069eeb6bfc2
Antonio Lima <antonio55senac@gmail.com>
2021-06-08 13:57:56 +0000

    Aula 23 - Removendo elementos de um array

---
+++ Aulas/aula23.js
@@ -0,0 +1,23 @@
+// (23) - Introdução a Arrays - Removendo Elementos /
+
+const numero = [1,2,3,4,5,6];
+numero.push();
+numero.unshift();
+numero.splice();
+
+
+//Remover elemento no início do array
+const primeiro = numero.shift();
+console.log(primeiro);
+console.log(numeros);
+
+//Remover elemento no meio do array
+const meio = numero.splice();
+console.log(meio);
+console.log(numeros);
+
+
+//Remover elemento no final do array
+const ultimo = numero.pop();
+console.log(ultimo);
+console.log(numeros);
