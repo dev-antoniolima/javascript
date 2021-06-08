c48de6e5eeab92340f077e01587204dece889311
Antonio Lima <antonio55senac@gmail.com>
2021-06-08 14:51:16 +0000

    Aula 25 - Combinando e cortando arrays

--- Aulas/aula23.js
+++ Aulas/aula23.js
@@ -1,23 +1,22 @@
 // (23) - Introdução a Arrays - Removendo Elementos /
 
-const numero = [1,2,3,4,5,6];
-numero.push();
-numero.unshift();
-numero.splice();
+const numeros = [1,2,3,4,5,6];
+numeros.push();
+numeros.unshift();
+numeros.splice();
 
 
 //Remover elemento no início do array
-const primeiro = numero.shift();
+const primeiro = numeros.shift();
 console.log(primeiro);
 console.log(numeros);
 
 //Remover elemento no meio do array
-const meio = numero.splice();
+const meio = numeros.splice();
 console.log(meio);
 console.log(numeros);
 
-
 //Remover elemento no final do array
-const ultimo = numero.pop();
+const ultimo = numeros.pop();
 console.log(ultimo);
 console.log(numeros);
---
+++ Aulas/aula25.js
@@ -0,0 +1,12 @@
+// (25) - Introdução a Arrays - Combinando e cortando arrays /
+
+const primeiro = [1,2,3];
+const segundo = [4,5,6];
+
+// Combinar:
+const combinado = primero.concat(segundo);
+console.log(combinado);
+
+// Cortar:
+const cortado = combinado.slice(1,2);
+console.log(cortado);
