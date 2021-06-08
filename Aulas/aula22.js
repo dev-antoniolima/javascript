110b5bf530a58b5b450bd80cb5ff58afa704b552
Antonio Lima <antonio55senac@gmail.com>
2021-06-08 13:49:30 +0000

    Aula 22 - Arrow Functions

--- Aulas/aula22.js
+++ Aulas/aula22.js
@@ -1,6 +1,10 @@
 // (21) - Introdução a Arrays - Arrow Functions //
 
 const marcas = [
-    {id: 1, nome: 'Antonio'}
+    {id: 1, nome: 'Antonio'},
     {id: 2, nome: 'Lima'}
-]
+];
+
+// Mesma utilidade de uma function (=>)
+
+console.log(marcas.find((marca) => marca.nome === 'a'));
