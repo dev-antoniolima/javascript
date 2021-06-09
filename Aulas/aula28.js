c9efdd8f0a09fe3121cba5c28010d183e0ebd89e
Antonio Lima <antonio55senac@gmail.com>
2021-06-09 01:59:31 +0000

    Aula 28 - Combinando Arrays

---
+++ Aulas/aula28.js
@@ -0,0 +1,12 @@
+// (28) - Introdução a Arrays - Combinando Arrays //
+
+const numeros = [1,2,3,4];
+
+const combinado = numeros.join('.');
+console.log(combinado);
+
+const frase = 'Olá bem vindo ao curso';
+const resultado = frase.split(' ');
+
+console.log(resultado);
+console.log(resultado.join('.'));
