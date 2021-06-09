757e901581f45166f4870bb42e738c971ddbe3df
Antonio Lima <antonio55senac@gmail.com>
2021-06-09 02:04:54 +0000

    Aula 29 - Recebendo dados do usuário

---
+++ Aulas/aula29.js
@@ -0,0 +1,5 @@
+let corFavorita = prompt('Olá, qual é a sua cor favorita ?');
+if(corFavorita.value == 'vermelho'){
+    let exibirCor = prompt('Sua cor favorita é o: '+corFavorita)
+    console.log(exibirCor);
+}
