8480fb07c32eca98a33e00baf70fec42a7550707
Antonio Lima <antonio55senac@gmail.com>
2021-10-27 05:06:43 +0000

    Mini Projeto (15) - Contador de letra em frase

---
+++ Mini-Projetos/minPro15.js
@@ -0,0 +1,34 @@
+// // Mini Projeto(15) - Encontrar número de vezes que uma letra está presente na frase.
+
+var frase = "java";
+var letra = 'a';
+
+function contarLetra(frase, letra) {
+  var contador = 0
+
+  for (var i = 0; i < frase.length; i++) {
+    if (frase[i] == letra) {
+      contador++
+    }
+  }
+  return contador;
+}
+
+
+ "a";
+console.log("A letra  " + letra + "  apareceu: " + contarLetra(frase, letra) + " vezes na frase!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
